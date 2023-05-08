7efc6122ae884c2436a2fc47b26975dcffcea3a0
robert-Finke <98506671+robert-Finke@users.noreply.github.com>
2023-05-07 20:18:14 -0400

    Add files via upload

--- CustomRouter.py
+++ CustomRouter.py
@@ -11,10 +11,8 @@ from mininet.node import RemoteController, Controller, OVSKernelSwitch, Node, Ho
 from mininet.link import TCLink, Link
 from mininet.cli import CLI
 from mininet.util import dumpNodeConnections
-import packet
 import heapq
 import sys
-from scapy.all import *
 
 
 class UDPRouter(Node):
@@ -28,8 +26,6 @@ class UDPRouter(Node):
         super(UDPRouter, self).config(**params)
         self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    # this has to be changed, we can't use the net topology in the implementation of the routers, as we don't know what it is. 
-    # We have to discover topology with our routing protocol
     def get_links(self, net):
         links = {}
         for intf in self.intfList():
@@ -64,7 +60,6 @@ class UDPRouter(Node):
 
         return distance
 
-    #this also has to be changed, we can't use the net 
     def compute_routing_tables(self, net):
         graph = {}
         for n in net.hosts:
@@ -73,8 +68,8 @@ class UDPRouter(Node):
         self.routing_table = self.compute_shortest_paths(graph, self.name)
 
     def send_packet(self, packet):
-        header = read_header(packet)
-        dest = header["dst"]
+        header = self.read_header(packet)
+        dest = header['dst']
         if dest in self.routing_table:
             nexthop = self.routing_table[dest]
             out_intf = None
@@ -93,46 +88,10 @@ class UDPRouter(Node):
             info(f"{self.name}: Destination {dest} not found in routing table\n")
 
     def receive_packet(self, packet):
-        header = read_header(packet) # get header in order to process packet
-        
-        if header["dst"] == self.ip: #if the packet's destination is the router
+        header = self.read_header(packet)
+        if header['dst'] == self.ip:
             info(f"{self.name}: Packet received for local processing\n")
             # Process packet here
-            #check what type it is
-            if(header.type == 1): 
-                #hello packet
-                print(1)
-            elif(header.type == 2):
-                #hello ACK packet
-                print(2)
-            elif(header.type == 3):
-                #multicast packet
-                print(3)
-            elif(header.type == 4):
-                #unicast packet
-                #check if this router is the intended destination
-                #if it is, we also have to check if the data field contains a multicast packet
-                data = read_data(packet)
-
-                if(data[0] == 3): #if the first number in the data is a 3, it is the type field of a multicast packet
-                    #now we have to split the packets to unicast, and determine k closest destinations
-                    multiHeader = read_header(data)
-                    multiData = read_data(data)
-
-                    destList = [] # this will be an array containing all 3 destinations, sorted by closest first. We will have to implement a method.
-
-                    # send k packets to destinations. Currently "src" field is the original src where the multicast packet was sent from, SEQ = 1, & TTL = 10
-                    for i in range(0, multiHeader["kval"]):
-                        send_packet(self, create_unicast_packet(1, 10, header["src"], destList[i], multiData))
-                else:
-                    print(data)
-
-                print(4)
-            elif(header.type == 5):
-                #LSA packet
-                print(5)
-            else:
-                print("error!")
         else:
             self.packet_queue.append(packet)
 
@@ -141,8 +100,6 @@ class UDPRouter(Node):
             self.send_packet(packet)
         self.packet_queue = []
 
-
-if __name__ == '__main__':
-    print("Router Started...")
-    udp_router = UDPRouter("router", "10.0.0.1")
+    def read_header(self, packet):
+        # Replace this with the appropriate function
 
